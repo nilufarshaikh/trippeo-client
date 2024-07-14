096ea93dd5447394d23967b77f086595dbd20bfe
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-07-14 10:45:58 -0400

    Added search ui for desktop

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,9 +10,13 @@ import logo from "../../assets/logo/logo.png";
 import "./Navigation.scss";
 import { useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
   const username = sessionStorage.getItem("username");
   const navigate = useNavigate();
 
@@ -26,6 +30,31 @@ const Navigation = () => {
     navigate("/login");
   };
 
+  const toggleSearchResultBox = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    const searchUserURL = `${
+      import.meta.env.VITE_API_URL
+    }/auth/search?query=${query}`;
+
+    try {
+      const token = sessionStorage.getItem("token");
+      const response = await axios.get(searchUserURL, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setResults(response.data);
+      setIsSearchOpen(true);
+    } catch (error) {
+      console.error("There was an error submitting the story:", error);
+    }
+  };
+
   return (
     <nav className="navbar">
       <Link
@@ -37,17 +66,52 @@ const Navigation = () => {
       <Link className="navbar__home-nav-link" to="/home">
         <HomeOutlinedIcon className="navbar__icons" />
       </Link>
-      <form className="search-form" id="searchForm">
+
+      <form className="search-form" onSubmit={handleSearchSubmit}>
         <SearchOutlinedIcon className="search-form__icon navbar__icons" />
-        <input
-          className="search-form__input"
-          type="text"
-          placeholder="Search..."
-          name="search"
-          id="search"
-        />
-        <button className="search-form__button">Search</button>
+        <div className="search-inputs">
+          <input
+            className="search-form__input"
+            type="text"
+            placeholder="Search for users..."
+            name="search"
+            id="search"
+            required
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="search-form__button">Search</button>
+        </div>
       </form>
+
+      {isSearchOpen && (
+        <>
+          <ul className="profile-nav__search-list">
+            {results.length > 0 ? (
+              results.map((user) => (
+                <div key={user._id} className="user-card">
+                  <div className="avatar search-avatar">
+                    <img
+                      className="avatar__image"
+                      src={user.profilePicture}
+                      alt="Profile photo"
+                    />
+                  </div>
+                  <h3>{user.username}</h3>
+                  <p className="user-card__bio">{user.bio}</p>
+                  <button className="user-card__btn">Follow</button>
+                </div>
+              ))
+            ) : (
+              <p>No results found</p>
+            )}
+            <p className="search-close" onClick={toggleSearchResultBox}>
+              Close
+            </p>
+          </ul>
+        </>
+      )}
+
       <NavLink className="create-story" to="/stories/add">
         <AddOutlinedIcon className="navbar__icons create-story__icon" />
       </NavLink>
